3c23ac7a54b5f2564a27e4ba9c5a34c6afbc1604
Matthias Petri <mpetri@fastmail.com>
2022-12-22 17:54:00 -0800

    Update extract.rs

--- src/extract.rs
+++ src/extract.rs
@@ -184,13 +184,13 @@ pub trait Extractor {
 
     /// When the article was published (and last updated).
     fn publishing_date(&self, doc: &Document, base_url: Option<&Url>) -> Option<ArticleDate> {
-        if let Some(date) = DateExtractor::extract_from_doc(doc) {
-            return Some(date);
-        }
+//         if let Some(date) = DateExtractor::extract_from_doc(doc) {
+//             return Some(date);
+//         }
 
-        if let Some(url) = base_url {
-            return DateExtractor::extract_from_str(url.path());
-        }
+//         if let Some(url) = base_url {
+//             return DateExtractor::extract_from_str(url.path());
+//         }
 
         None
     }
